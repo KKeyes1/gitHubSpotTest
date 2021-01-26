40d9747767c75521b8c7bef23082145e0db16cde
wh015k3n <keyes@Kenneths-MacBook-Pro.local>
2021-01-26 13:56:37 -0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [/modules/tailwindTestModule.module/module.html],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
